74f5c45661922cdbb703cfa0e84ed1115f6cd013
Andreas Nenner <andreas@zensai.io>
2024-04-23 10:41:41 +0200

    fixes list_templates to look at custom vaults

--- src/template_utils.py
+++ src/template_utils.py
@@ -1,8 +1,8 @@
 from .vault.vault_factory import create_vault
 
-def get_template(template_name, version=None):
+def get_template(template_name, version=None, **kwargs):
     vault = create_vault()
-    return vault.load(template_name, version)
+    return vault.load(template_name, version, **kwargs)
 
 
 def save_template(template, **kwargs):
@@ -10,6 +10,6 @@ def save_template(template, **kwargs):
     vault.save(template, **kwargs)
 
 
-def list_templates():
+def list_templates(**kwargs):
     vault = create_vault()
-    return vault.list_templates()
+    return vault.list_templates(**kwargs)
